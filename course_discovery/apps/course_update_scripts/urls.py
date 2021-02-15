ef746b9f8679b07d97fe36fca18b364a526e3be4
Neeraj Singh <neeraj.kumar@manprax.com>
2021-02-15 16:29:54 +0530

    change sink to sync

--- course_discovery/apps/course_update_scripts/urls.py
+++ course_discovery/apps/course_update_scripts/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', update_scripts, name="update_scripts"),
-    url(r'^course-sink$', discovery_views, name="course_sink"),
+    url(r'^course-sync$', discovery_views, name="course_sync"),
     url(r'^update-index$', update_indexCmd, name='update_indexCmd')
 ]
